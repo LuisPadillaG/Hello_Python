0681b0b9ff6e55855ea05c9867a287c620b4056e
LuisPadillaG <luis.gle3@gmail.com>
2025-06-14 12:50:29 -0600

    [AGREGA] proyecto de prints en python

---
+++ 01_BienvenidoAPython/01_print.py
@@ -0,0 +1,9 @@
+###Modulo print es para imprimir en la consola
+#Nos va a acompañar de aquí hasta el infinito
+print("¡Hola gente!")
+print('Hola "comillas" simples')
+
+print("Python", "Es", "Genial", sep="¿¿¿")
+
+print("Esto se imprime", end=" ")
+print("en la misma linea")
