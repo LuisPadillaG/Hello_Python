08480d3a715103786ba771b76bf255c7a7668e81
LuisPadillaG <luis.gle3@gmail.com>
2025-06-15 01:14:33 -0600

    AGREGA proyecto de tiposdedatos

---
+++ 02_TiposDeDatos/02_types.py
@@ -0,0 +1,29 @@
+###
+#Python tiene varios tipos de datos
+# int, float, str, complex, bool, NoneType, list, tuple, dict, range, set...
+###
+
+print("int:")
+#print(type(10))
+#print(type(5))
+#print(type(-10))
+#print(type(12851213413414579975959))
+
+print("float:")
+#print(type(3.1416))
+#print(type(0.14116))
+#print(type(1e3)) #notacion cientifica
+
+print("numeros complejos (complex):")
+#print(type(1 + 2j))
+
+print("str:")
+#print(type("Hola"))
+#print(type(""))
+#print(type("" \
+#"Hola" \
+#""))
+
+print("bool:")
+print(type(True))
+print(type(False))
