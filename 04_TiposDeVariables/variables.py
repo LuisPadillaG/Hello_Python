80f22f8ade231c68bbabff139b455d8e59a55ae7
LuisPadillaG <luis.gle3@gmail.com>
2025-07-03 15:56:53 -0600

    {TERMINA} codigo 04 + Tema 05 en el mismo codigo 04

--- 04_TiposDeVariables/variables.py
+++ 04_TiposDeVariables/variables.py
@@ -19,3 +19,29 @@ print(type(my_name))
 my_name = 20
 print(type(my_name))
 
+my_name_real = "Luis"
+#Tipado fuerte: Python no realiza conversiones automáticas
+#print(10 + "2") #no funciona obvio
+#Si quieres evaluar un numero de un texto puedes
+print(f"Hola soy {my_name_real}, tengo {my_name + 1} anios")
+
+
+n, age, city = "Alberto", 23, "Caracas" # No recomendada forma de asignar variables
+
+# Convenciones de nombres de variable
+mi_nombre_de_variable = "ok"
+
+MI_CONSTANTE = 31.4 #Las UPPER_CASE se pensaron para constantes, realmente no existen constantes en python, pero allí te informo
+print(MI_CONSTANTE)
+
+### ---------------------------------------------------------------------
+### ---------------------------------------------------------------------
+### ---------------------------------------------------------------------
+##05 AGREGAR TIPADO DE VARIABLES
+### ---------------------------------------------------------------------
+
+is_user_logged_in: bool = True
+print(is_user_logged_in)
+
+is_user_logged_in = 2424
+print(is_user_logged_in)
